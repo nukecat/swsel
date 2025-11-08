85bd8a41d73643b7c2760a4dbf160d08884c8734
nukcat <unclearcat@gmail.com>
2025-11-08 02:18:51 +0100

    feat: change new() method to accept string as type identifier for Block (see block.rs, forg. to incl.)

--- src/definitions/mod.rs
+++ src/definitions/mod.rs
@@ -3,6 +3,7 @@ pub mod structs;
 use once_cell::sync::Lazy;
 use serde_cbor::from_slice;
 use structs::*;
+use indexmap::IndexMap;
 
 
 #[cfg(not(any(rust_analyzer, doc)))]
@@ -11,10 +12,14 @@ static BLOCKS: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/definitions.cbo
 #[cfg(any(rust_analyzer, doc))]
 static BLOCKS: &[u8] = &[]; // = include_bytes!(concat!(env!("OUT_DIR"), "/definitions.cbor"));
 
-static BLOCK_DEFINITIONS: Lazy<BlockDefinitionsFile> = Lazy::new(|| {
+static BLOCK_DEFINITIONS_CBOR: Lazy<BlockDefinitionsFile> = Lazy::new(|| {
     from_slice(BLOCKS).expect("Failed to parse definitions.cbor")
 });
 
-pub fn get_block_definitions() -> &'static Vec<BlockDefinition> {
-    &BLOCK_DEFINITIONS.blocks
-}
+pub static BLOCK_NAMES_IDS_MAP: Lazy<IndexMap<&str, u8>> = Lazy::new(|| {
+    let mut map: IndexMap<&str, u8> = IndexMap::new();
+    for block in BLOCK_DEFINITIONS_CBOR.blocks.iter() {
+        map.insert(&block.name, block.id);
+    }
+    map
+});
